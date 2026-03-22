6e0bc9e67cfc7cd2d6ccf76899303a348bbb178e
ntgptit <56243056+ntgptit@users.noreply.github.com>
2026-03-22 19:15:23 +0900

    Add backend service

---
+++ memora-api-service/src/main/java/com/memora/app/MemoraApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.memora.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoraApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoraApiServiceApplication.class, args);
+	}
+
+}
